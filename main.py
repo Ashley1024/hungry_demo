ecbcb38e9f92bfb377755a958fe5314ab17ab8bf
Jane <1205517586@qq.com>
2019-11-15 20:06:52 +0800

    11.15

---
+++ main.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+# application,main(last line) function only one in a file is entrance of an application
+hungry = Flask(__name__)
+
+if __name__ == '__main__':
+    print(__name__)
+    hungry.run(debug=True)

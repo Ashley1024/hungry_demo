870e61880c6f007dbe90d726173f91a44e993f0c
Jane <1205517586@qq.com>
2019-11-15 21:54:28 +0800

    no message

---
+++ hungry/models.py
@@ -0,0 +1,15 @@
+# 这是一个模型的例子（将代码放如models.py或类似文件中）
+from sqlalchemy import Column, Integer,String
+
+class User(db.Model):
+    __tablename__ ='users'
+    id = Column(Integer,primary_key=True)
+    name =Column(String(50),unique=True)
+    email =Column(String(120),unique=True)
+
+    def __init__(self, name=None,email=None):
+        self.name = name
+        self.email =email
+
+    def __repr__(self):
+        return '<User %r>' %(self.name)
